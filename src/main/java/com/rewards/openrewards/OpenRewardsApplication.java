c8a6bfe403275fe69f366d31641a66f5be11ea52
correia <leandro.correia@tradepro.com.br>
2026-01-18 16:48:35 -0300

    initial commit

---
+++ src/main/java/com/rewards/openrewards/OpenRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.rewards.openrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRewardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenRewardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rewards/openrewards/OpenRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewards.openrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRewardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
